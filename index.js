cc38ac7ea82d71fb3897df2c502a6710fac8a487
hengin-eer <hirokix0608@gmail.com>
2023-10-15 15:27:10 +0900

    ♻️ refactor: リファクタリング

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ let beta = 0;
 let gamma = 0;
 
 function handleOrientation(event) {
-    alpha = parseInt(event.alpha.toFixed(2));
-    beta = parseInt(event.beta.toFixed(2));
-    gamma = parseInt(event.gamma.toFixed(2));
+    alpha = parseInt(event.alpha);
+    beta = parseInt(event.beta);
+    gamma = parseInt(event.gamma);
 
     compass.innerHTML = `Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
+
     if (alpha > 45 && alpha <= 135) direction.innerHTML = "East";
     else if (alpha > 135 && alpha <= 225) direction.innerHTML = "South";
     else if (alpha > 225 && alpha <= 315) direction.innerHTML = "West";
