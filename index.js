3ea64396084355f1a2c6bf10f004d0074cee38be
hengin-eer <hirokix0608@gmail.com>
2023-10-15 14:56:33 +0900

    ✨ 方角を表示する処理の追加

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const compass = document.querySelector("#orientation");
+const direction = document.querySelector("#direction");
+
 let alpha = 0;
 let beta = 0;
 let gamma = 0;
@@ -9,6 +11,23 @@ function handleOrientation(event) {
     gamma = event.gamma.toFixed(2);
 
     compass.innerHTML = `Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
+    switch (alpha) {
+        case (alpha > 45 && alpha <= 135):
+            direction.innerHTML = "East";
+            break;
+
+        case (alpha > 135 && alpha <= 225):
+            direction.innerHTML = "South";
+            break;
+
+        case (alpha > 225 && alpha <= 315):
+            direction.innerHTML = "West";
+            break;
+    
+        default:
+            direction.innerHTML = "North";
+            break;
+    }
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
