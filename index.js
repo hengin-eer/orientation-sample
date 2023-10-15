6e680642147c76900745e0c3e068cbc50db4cc7f
hengin-eer <hirokix0608@gmail.com>
2023-10-15 15:16:02 +0900

    ✨ feat: 修正その4

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function handleOrientation(event) {
     gamma = parseInt(event.gamma.toFixed(2));
 
     compass.innerHTML = `Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
-    if (alpha > 315 <= 360 || 0 > alpha <= 45) direction.innerHTML = "North";
-    else if (alpha > 45 && alpha <= 135) direction.innerHTML = "East";
+    if (alpha > 45 && alpha <= 135) direction.innerHTML = "East";
     else if (alpha > 135 && alpha <= 225) direction.innerHTML = "South";
     else if (alpha > 225 && alpha <= 315) direction.innerHTML = "West";
+    else direction.innerHTML = "North";
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
