b8acf21141a2b5626f0055209aaa2703e77421ed
hengin-eer <hirokix0608@gmail.com>
2023-10-18 23:23:27 +0900

    ✨ feat: コンパスの回転処理を追加、方角の変数名を修正

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const compass = document.querySelector("#orientation");
+const orientationItem = document.querySelector("#orientation");
 const direction = document.querySelector("#direction");
+const compass = document.querySelector("#compass");
 
 let alpha = 0;
 let beta = 0;
@@ -10,12 +11,14 @@ function handleOrientation(event) {
     beta = parseInt(event.beta);
     gamma = parseInt(event.gamma);
 
-    compass.innerHTML = `Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
+    orientationItem.innerHTML = `Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
 
     if (alpha > 45 && alpha <= 135) direction.innerHTML = "East";
     else if (alpha > 135 && alpha <= 225) direction.innerHTML = "South";
     else if (alpha > 225 && alpha <= 315) direction.innerHTML = "West";
     else direction.innerHTML = "North";
+
+    compass.style.transform = `rotate(-${alpha}deg)`;
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
